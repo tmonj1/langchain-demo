1ede2e07f87b3228a42c0ca4418e5e8713ed0a54
tmonj1 <tmonj1@gmail.com>
2025-03-06 00:11:30 +0900

    [wip] update 9 (Chrome DB Viewer)

--- src/9_langchain.py
+++ src/9_langchain.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import ttk, filedialog
+from attr import AttrsInstance, attributes
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 
@@ -56,35 +57,48 @@ class ChromaApp:
         window_height = int(screen_height * 0.7)
         root.geometry(f"{window_width}x{window_height}")
 
+        #
+        # 上段のフレーム
+        #
+
         # Frame を使ってマージンを確保
-        frame = tk.Frame(self.root, padx=20, pady=20)  # 余白を設定
-        frame.pack(fill="both", expand=True)
+        frame_top = tk.Frame(self.root, height=30, padx=10, pady=0)  # 余白を設定
+        frame_top.pack(fill="x")
+        frame_top.pack_propagate(True)
 
-        # データベースファイル名の表示
-        self.db_label = tk.Label(frame, text=f"Database File: {db_path}")
-        self.db_label.pack(anchor='w')
+        # フレームのカラムを設定（ラベルの幅を統一）
+        frame_top.columnconfigure(0, minsize=100)  # ラベル列の幅を100pxに固定
+        frame_top.columnconfigure(1, weight=1)  # Entry をウィンドウ幅に応じて拡張
 
-        # コレクション名の表示
-        self.db_collection_label = tk.Label(frame, text=f"Collection: {chroma_db._collection_name}")
-        self.db_collection_label.pack(anchor='w')
+        # ラベルとテキストボックスを配置
+        label_texts = [
+            "Database File:",
+            "Collection:",
+            "Total Documents:",
+            "Enter Document Number:"
+        ]
+        entry_attributes = [
+            {"state": "readonly", "stickey": "ew", "value": f"{db_path}"},
+            {"state": "readonly", "stickey": "ew", "value": f"{chroma_db._collection_name}"},
+            {"state": "readonly", "stickey": "w",  "value": f"{len(doc_ids)}"},
+            {"state": "normal",   "stickey": "w",  "value": ""}
+        ]
+        entries = []
 
-        # データの総件数の表示
-        self.count_label = tk.Label(frame, text=f"Total Documents: {len(doc_ids)}")
-        self.count_label.pack(anchor='w')
+        for i, label_text in enumerate(label_texts):
+            label = tk.Label(frame_top, text=label_text, anchor="w")
+            label.grid(row=i, column=0, sticky="w", padx=5, pady=0)
 
-        # 番号指定用のテキストボックス
-        frame_number_entry = tk.Frame(frame)
-        frame_number_entry.pack(anchor='w')
-        self.entry_label = tk.Label(frame_number_entry, text="Enter Document Number:")
-        self.entry_label.pack(side='left')
-        vcmd = (root.register(self.validate_entry), '%P')
-        self.entry = tk.Entry(
-            frame_number_entry,
-            validate='key',
-            validatecommand=vcmd,
-            width=5,
-            justify='right')
-        self.entry.pack(side='left', fill='x', expand=True)
+            attributes = entry_attributes[i]
+            entry = tk.Entry(
+                frame_top,
+                textvariable=tk.StringVar(value=attributes["value"]),
+                state=attributes["state"])
+            entry.grid(row=i, column=1, sticky=attributes["stickey"], padx=5, pady=0)
+            entries.append(entry)
+
+        doc_number_entry = entries[-1]
+        self.entry = doc_number_entry
 
         # キーイベントのバインド
         self.entry.bind("<Up>", self.increment_entry)
@@ -92,72 +106,125 @@ class ChromaApp:
         self.entry.bind("<Return>", lambda event: self.search_document())  # Enterキーのバインド
         self.entry.bind("<KeyRelease>", lambda event: self.search_document())  # キーリリースイベントのバインド
 
-        # 区切り線
-        separator = ttk.Separator(frame, orient="horizontal")
-        separator.pack(fill="x", padx=10, pady=10)
+        #
+        # 中段のフレーム
+        #
+        frame_middle = tk.Frame(self.root, padx=10, pady=0)
+        frame_middle.pack(fill="x")
+
+        ## 区切り線
+        separator = ttk.Separator(frame_middle, orient="horizontal")
+        separator.pack(fill="x", padx=10, pady=5)
+
+        #
+        # 下段のフレーム
+        #
+        frame_bottom = tk.Frame(self.root, padx=10, pady=0)
+        frame_bottom.pack(fill="both", expand=True)
+
+        # フレームのカラムを設定（ラベルの幅を統一）
+        frame_top.columnconfigure(0, minsize=100)  # ラベル列の幅を100pxに固定
+        frame_top.columnconfigure(1, weight=1)  # Entry をウィンドウ幅に応じて拡張
+
+        label_texts2 = [
+            "Document ID:",
+            "Embedding:",
+            "Result Length:",
+            "Search Result:"
+        ]
+
+        entry_attributes2 = [
+            {"state": "readonly", "stickey": "w", "value": "-"},
+            {"state": "readonly", "stickey": "w", "value": "-"},
+            {"state": "readonly", "stickey": "w", "value": "0"},
+            {"state": "readonly", "stickey": "w", "value": ""}
+        ]
+        entries2 = []
+
+        for i, label_text in enumerate(label_texts2):
+            label = tk.Label(frame_bottom, text=label_text, anchor="w")
+            label.grid(row=i, column=0, sticky="w", padx=5, pady=0)
+
+            attributes = entry_attributes2[i]
+            entry = tk.Entry(
+                frame_bottom,
+                textvariable=tk.StringVar(value=attributes["value"]),
+                state=attributes["state"])
+            entry.grid(row=i, column=1, sticky=attributes["stickey"], padx=5, pady=0)
+            entries2.append(entry)
+
+
+
+        #$$$ Todo's
+        #* frame_topはDBが変わったときに更新するようにする
+        #  * update_database_frame(db_info) みたいな関数を作って、それを呼ぶようにする
+        #* frame_bottomはdoc_number_entryが変わったときに更新するようにする
+        #  * update_document_frame(doc_info) みたいな関数を作って、それを呼ぶようにする
+
+
 
         # ドキュメントIDの表示
-        frame_doc_id = tk.Frame(frame)
-        frame_doc_id.pack(anchor='w', fill='x')
-        self.doc_id_label = tk.Label(frame_doc_id, text="Document ID: ")
-        self.doc_id_label.pack(side='left')
-        self.doc_id_var = tk.StringVar(value="-")
-        self.doc_id_entry = tk.Entry(
-            frame_doc_id,
-            textvariable=self.doc_id_var,
-            state="readonly",
-            width=20,
-            justify='left')
-        self.doc_id_entry.pack(side='left', fill='x', expand=True)
+        #frame_doc_id = tk.Frame(frame_bottom)
+        #frame_doc_id.pack(anchor='w', fill='x')
+        #self.doc_id_label = tk.Label(frame_doc_id, text="Document ID: ")
+        #self.doc_id_label.pack(side='left')
+        #self.doc_id_var = tk.StringVar(value="-")
+        #self.doc_id_entry = tk.Entry(
+        #    frame_doc_id,
+        #    textvariable=self.doc_id_var,
+        #    state="readonly",
+        #    width=20,
+        #    justify='left')
+        #self.doc_id_entry.pack(side='left', fill='x', expand=True)
 
         # 埋め込みの表示
-        frame_embedding = tk.Frame(frame)
-        frame_embedding.pack(anchor='w', fill='x')
-        self.embedding_dimension_var = tk.StringVar(value="Embbeding (Array)")
-        self.embedding_label = tk.Label(frame_embedding, textvariable=self.embedding_dimension_var)
-        self.embedding_label.pack(side='left')
-        self.embedding_var = tk.StringVar(value="-")
-        self.embedding_entry = tk.Entry(
-            frame_embedding,
-            textvariable=self.embedding_var,
-            state="readonly",
-            width=20,
-            justify='left')
-        self.embedding_entry.pack(side='left', fill='x', expand=True)
+        #frame_embedding = tk.Frame(frame_bottom)
+        #frame_embedding.pack(anchor='w', fill='x')
+        #self.embedding_dimension_var = tk.StringVar(value="Embbeding (Array)")
+        #self.embedding_label = tk.Label(frame_embedding, textvariable=self.embedding_dimension_var)
+        #self.embedding_label.pack(side='left')
+        #self.embedding_var = tk.StringVar(value="-")
+        #self.embedding_entry = tk.Entry(
+        #    frame_embedding,
+        #    textvariable=self.embedding_var,
+        #    state="readonly",
+        #    width=20,
+        #    justify='left')
+        #self.embedding_entry.pack(side='left', fill='x', expand=True)
 
-        # テキスト長の表示
-        frame_result_length = tk.Frame(frame)
-        frame_result_length.pack(anchor='w')
-        self.result_length_label = tk.Label(frame_result_length, text="Result Length:")
-        self.result_length_label.pack(side='left')
-        self.result_length_var = tk.StringVar(value="0")
-        self.result_length_entry = tk.Entry(
-            frame_result_length,
-            textvariable=self.result_length_var,
-            state="readonly",
-            width=10,
-            justify='left')
-        self.result_length_entry.pack(side='left', fill='x', expand=True)
+        ## テキスト長の表示
+        #frame_result_length = tk.Frame(frame_bottom)
+        #frame_result_length.pack(anchor='w')
+        #self.result_length_label = tk.Label(frame_result_length, text="Result Length:")
+        #self.result_length_label.pack(side='left')
+        #self.result_length_var = tk.StringVar(value="0")
+        #self.result_length_entry = tk.Entry(
+        #    frame_result_length,
+        #    textvariable=self.result_length_var,
+        #    state="readonly",
+        #    width=10,
+        #    justify='left')
+        #self.result_length_entry.pack(side='left', fill='x', expand=True)
 
-        # 結果表示用のテキストボックス
-        frame_search_result = tk.Frame(frame)
-        frame_search_result.pack(anchor='w', fill='both', expand=True)
-        self.result_text_label = tk.Label(frame_search_result, text="Search Result:")
-        self.result_text_label.pack(side='left', anchor='n')
-        self.result_text = tk.Text(frame_search_result, height=10, width=50, wrap='word')
-        self.result_text.pack(side='left', fill='both', expand=True)
+        ## 結果表示用のテキストボックス
+        #frame_search_result = tk.Frame(frame_bottom)
+        #frame_search_result.pack(anchor='w', fill='both', expand=True)
+        #self.result_text_label = tk.Label(frame_search_result, text="Search Result:")
+        #self.result_text_label.pack(side='left', anchor='n')
+        #self.result_text = tk.Text(frame_search_result, height=10, width=50, wrap='word')
+        #self.result_text.pack(side='left', fill='both', expand=True)
 
-        # 縦スクロールバーの追加
-        self.scrollbar = tk.Scrollbar(self.result_text, command=self.result_text.yview)
-        self.scrollbar.pack(side='right', fill='y')
+        ## 縦スクロールバーの追加
+        #self.scrollbar = tk.Scrollbar(self.result_text, command=self.result_text.yview)
+        #self.scrollbar.pack(side='right', fill='y')
 
-        # テキストボックスにスクロールバーを設定
-        self.result_text.config(yscrollcommand=self.scrollbar.set)
+        ## テキストボックスにスクロールバーを設定
+        #self.result_text.config(yscrollcommand=self.scrollbar.set)
 
-        # データが存在する場合は最初のドキュメントを表示
-        if len(doc_ids) > 0:
-            self.entry.insert(0, "0")
-            self.search_document()
+        ## データが存在する場合は最初のドキュメントを表示
+        #if len(doc_ids) > 0:
+        #    self.entry.insert(0, "0")
+        #    self.search_document()
 
     def open_chroma_database(self):
         directory_path = filedialog.askdirectory()
